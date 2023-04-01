aeb3a454cac4b843d324cdd1ec56fc329d6fb5a0
AmyHMSmith <amyhmsmith@gmail.com>
2023-04-01 18:55:04 +0100

    itinerary added

--- __tests__/Port.test.js
+++ __tests__/Port.test.js
@@ -1,5 +1,6 @@
 const Port = require('../src/Port.js');
 
+
 describe('Port', () => {
     it('can be instantiated', () => {
         expect(new Port()).toBeInstanceOf(Object);
--- __tests__/Ship.test.js
+++ __tests__/Ship.test.js
@@ -1,21 +1,29 @@
 const Ship = require('../src/Ship.js');
 const Port = require('../src/Port.js');
+const Itinerary = require('../src/Itinerary.js');
 
 describe('Ship', () => {
     it('can be instantiated', () => {
-        expect(new Ship()).toBeInstanceOf(Object);
+        const port = new Port('Portsmouth');
+        const itinerary = new Itinerary([port]);
+        const ship = new Ship(itinerary);
+
+        expect(ship).toBeInstanceOf(Object);
     });
 
     it('has a starting port', () => {
         const port = new Port('Portsmouth');
-        const ship = new Ship(port);
+        const itinerary = new Itinerary([port]);
+        const ship = new Ship(itinerary);
 
         expect(ship.currentPort).toBe(port);
     });
 
     it('can set sail', () => {
-        const port = new Port('Portsmouth');
-        const ship = new Ship(port);
+        const portsmouth = new Port('Portsmouth');
+        const ilfracombe = new Port('Ilfracombe');
+        const itinerary = new Itinerary([portsmouth, ilfracombe]);
+        const ship = new Ship(itinerary);
 
         ship.setSail();
 
@@ -24,11 +32,25 @@ describe('Ship', () => {
 
     it('can dock at different port', () => {
         const portsmouth = new Port('Portsmouth');
-        const ship = new Ship(portsmouth);
-
         const ilfracombe = new Port('Ilfracombe');
-        ship.dock(ilfracombe);
+        const itinerary = new Itinerary([portsmouth, ilfracombe]);
+        const ship = new Ship(itinerary);
+
+        ship.setSail();
+        ship.dock();
 
         expect(ship.currentPort).toBe(ilfracombe);
     });
+
+    it('cannot sail further than its itinerary', () => {
+        const portsmouth = new Port('Portsmouth');
+        const ilfracombe = new Port('Ilfracombe');
+        const itinerary = new Itinerary([portsmouth, ilfracombe]);
+        const ship = new Ship(itinerary);
+      
+        ship.setSail();
+        ship.dock();
+      
+        expect(() => ship.setSail()).toThrowError('-- End of itinerary --');
+      });
 });
